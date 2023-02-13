2449a23957e6b301f49e68790fe44957365264c6
philipp <philipp@freilinger.org>
2023-02-13 21:34:24 +0100

    fixed examples

--- get_json.php
+++ get_json.php
@@ -20,4 +20,7 @@ $posts =  $API->get_search_results($search_term);
 
 //get post by hastag. Example Tag would be #xbox. Needs to start with an '#'
 $posts =  $API->get_posts_by_tag($tag);
+
+//get a list of all used tags
+$tag_list =  $API->get_tag_list();
 ?>
